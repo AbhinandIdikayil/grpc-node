2d2514bf73c1aa5963e931d4cc8ca1cf02652259
Abhinand <abhin4nd6@gmail.com>
2024-06-14 12:10:24 +0530

    added payment service

---
+++ payment/payment.js
@@ -0,0 +1,24 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const path = require('path');
+
+
+const packageDefinition = protoLoader.loadSync(path.join(__dirname, '../protos/payment.proto'))
+
+const paymentProto = grpc.loadPackageDefinition(packageDefinition).payment
+
+
+const server = new grpc.Server();
+
+server.addService(paymentProto.PaymentService.service, {
+    ProcessPayment: (call, callback) => {
+        const {order_id , price} = call.request;
+        const success = true;
+        const message = 'payment successfull'
+        callback(null , {success , message})
+    }
+})
+
+server.bindAsync('0.0.0.0:50052',grpc.ServerCredentials.createInsecure(),() => {
+    server.start();
+})
